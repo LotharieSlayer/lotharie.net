ba606d2d6870fb086dd189b9d083d456d736b64b
Lotharie <49253492+LotharieSlayer@users.noreply.github.com>
2024-04-03 02:00:27 +0200

    feat: v0.1.0

--- app/components/LinkContainer/LinkContainer.tsx
+++ app/components/LinkContainer/LinkContainer.tsx
@@ -2,15 +2,18 @@
 
 import Image from "next/image";
 import styles from "./LinkContainer.module.css";
+import { useState } from "react";
 
 // LinkContainer component with href and name props
 export default function LinkContainer({
 	href,
 	name,
+	disabled = false,
 	newTab = false,
 }: {
 	href: string;
 	name: string;
+	disabled?: boolean;
 	newTab?: boolean;
 }) {
 
@@ -18,6 +21,8 @@ export default function LinkContainer({
 	let sfx_hover = "/sounds/hover.wav";
 	let sfx_loading = "/sounds/loading.wav";
 	let currentSound : HTMLAudioElement;
+
+	const [blink, setBlink] = useState(false);
 
 	function playSound(sound: string) {
 		currentSound = new Audio(sound);
@@ -33,34 +38,55 @@ export default function LinkContainer({
 	}
 
 	function go() {
+		// faire clignoter le lien en passant la police en blanc
+		setBlink(true);
 		playSound(sfx_go);
 		setTimeout(() => {
-			window.location.href = href;
+			if(newTab) {
+				window.open(href, "_blank");
+			}
+			else {
+				window.location.assign(href);
+			}
 		}, 300);
+
+		setTimeout(() => {
+			setBlink(false);
+		}, 500);
 	}
 
 	return (
 		<div className={styles.link_container}>
-			{ newTab ? 
-				<a
-					className={styles.link_text}
-					onClick={() => go()}
-					onMouseOver={() => playSound(sfx_hover)}
-					onMouseLeave={() => stopSound()}
-					target="_blank"
-				>
-					{name}
-				</a>
+			{ disabled ?
+				<div className={styles.link_text_disabled}>
+					{name} (soon)
+				</div>
 			:
-				<a
-					className={styles.link_text}
-					onClick={() => go()}
-					onMouseOver={() => playSound(sfx_hover)}
-					onMouseLeave={() => stopSound()}
-				>
-					{name}
-				</a>
+				<div>
+					{blink ?
+						<div className={styles.link_marquee}>
+							{renderLink()}
+						</div>
+					:
+					<div>
+						{renderLink()}
+					</div>
+					}
+				</div>
 			}
 		</div>
 	);
+
+	function renderLink() {
+		return (
+			<a
+				className={styles.link_text}
+				onClick={() => go()}
+				onMouseOver={() => playSound(sfx_hover)}
+				onMouseLeave={() => stopSound()}
+			>
+				{name}
+			</a>
+		);
+	}
 }
--- app/page.tsx
+++ app/page.tsx
@@ -8,15 +8,19 @@ export default function Home() {
 		<main className={styles.main}>
 			<Header title="home" />
 			<div className={styles.links_container}>
-				<LinkContainer href={"/dev"} name={"dev"}/>
-				<LinkContainer href={"/music"} name={"music"}/>
-				<LinkContainer href={"/video game"} name={"video game"}/>
-				<LinkContainer href={"/dj"} name={"dj"}/>
-				<LinkContainer href={"/uix"} name={"uix"}/>
+				{/* <LinkContainer href={"/dev"} name={"dev"}/> Temporary */}
+				<LinkContainer href={"https://www.github.com/LotharieSlayer"} name={"dev"} newTab={true}/>
+				<LinkContainer href={"/music"} name={"music"} disabled={true}/>
+				<LinkContainer href={"/video game"} name={"video game"} disabled={true}/>
+				<LinkContainer href={"/dj"} name={"dj"} disabled={true}/>
+				<LinkContainer href={"/uix"} name={"uix"} disabled={true}/>
 				<LinkContainer href={"https://www.speedrun.com/users/LOTHARIE"} name={"speedrun"} newTab={true}/>
 				<LinkContainer href={"https://francememes.com"} name={"france memes"} newTab={true}/>
 				<LinkContainer href={"https://jetrush.app"} name={"jetrush"} newTab={true}/>
 			</div>
+			<footer className={styles.footer}>
+				<p>© 2024 lotharie.net - v{process.env.npm_package_version}</p>
+			</footer>
 		</main>
 	);
 }
